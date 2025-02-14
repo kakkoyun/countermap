0601d6083e085f14e1b2463224874bb095d512f7
Kemal Akkoyun <kemal.akkoyun@datadoghq.com>
2025-02-14 17:35:21 +0100

    chore: Update Go toolchain and xsync
    Signed-off-by: Kemal Akkoyun <kemal.akkoyun@datadoghq.com>

--- xsync_map_counter_map.go
+++ xsync_map_counter_map.go
@@ -1,29 +1,26 @@
 package countermap
 
 import (
-	xsync "github.com/puzpuzpuz/xsync/v2"
+	xsync "github.com/puzpuzpuz/xsync/v3"
 )
 
 func NewXSyncMapCounterMap() *XSyncMapCounterMap {
-	return &XSyncMapCounterMap{counts: xsync.NewMap()}
+	return &XSyncMapCounterMap{counts: xsync.NewMapOf[string, *xsync.Counter]()}
 }
 
 type XSyncMapCounterMap struct {
-	counts *xsync.Map
+	counts *xsync.MapOf[string, *xsync.Counter]
 }
 
 func (cm *XSyncMapCounterMap) Inc(key string) {
-	val, ok := cm.counts.Load(key)
-	if !ok {
-		val, _ = cm.counts.LoadOrStore(key, xsync.NewCounter())
-	}
-	val.(*xsync.Counter).Add(1)
+	counter, _ := cm.counts.LoadOrStore(key, xsync.NewCounter())
+	counter.Add(1)
 }
 
 func (cm *XSyncMapCounterMap) GetAndReset() map[string]int64 {
 	ret := map[string]int64{}
-	cm.counts.Range(func(key string, val any) bool {
-		ret[key] = val.(*xsync.Counter).Value()
+	cm.counts.Range(func(key string, counter *xsync.Counter) bool {
+		ret[key] = counter.Value()
 		cm.counts.Delete(key)
 		return true
 	})
